ce4dd3e1eedf719d635042afeae5b962108df7b7
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-02-21 11:46:44 +0500

    addaed form-js by iskandar

---
+++ js/show-password.js
@@ -0,0 +1,17 @@
+let eye = document.querySelector(".form__password__input i");
+let passwordInput = document.querySelector("#password")
+
+eye.addEventListener("click",function(){
+    eye.classList.toggle("fa-eye-slash");
+    eye.classList.toggle("fa-eye");
+    
+
+    let type = passwordInput.getAttribute("type");
+    if(type==="password") {
+        passwordInput.setAttribute("type","text");
+
+    }
+    else{
+        passwordInput.setAttribute("type", "password");
+    }
+})
